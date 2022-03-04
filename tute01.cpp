06a6b07bdbdbac0d9663a99d3887daeaeb2281bd
IT21169380 <99633992+IT21169380@users.noreply.github.com>
2022-03-04 17:32:34 +0000

    tutorial 2 complete.

--- tute01.cpp
+++ tute01.cpp
@@ -17,14 +17,13 @@ printf("Hello World") --> std::cout << "Hello World";
 2.54cm = 1 inch
 */
 
-
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 int main() 
 {
     float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
+    cout<<"Enter a length in cm : "<<endl;
+    cin>>cm;
+    cout<<"Length in inches is = "<<cm/2.54<<" inches"<<endl;
     return 0;
 }  
